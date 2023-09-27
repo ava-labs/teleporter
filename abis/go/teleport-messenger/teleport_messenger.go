831babeffa426510371bfc36d6e6dcdc3bf13508
Gwen <linguan.yang@avalabs.org>
2023-09-27 13:55:32 -0400

    use subnet-evm abigen

--- abis/go/teleport-messenger/teleport_messenger.go
+++ abis/go/teleport-messenger/teleport_messenger.go
@@ -8,11 +8,11 @@ import (
 	"math/big"
 	"strings"
 
-	ethereum "github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ava-labs/subnet-evm/accounts/abi"
+	"github.com/ava-labs/subnet-evm/accounts/abi/bind"
+	"github.com/ava-labs/subnet-evm/core/types"
+	"github.com/ava-labs/subnet-evm/interfaces"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/event"
 )
 
@@ -21,7 +21,7 @@ var (
 	_ = errors.New
 	_ = big.NewInt
 	_ = strings.NewReader
-	_ = ethereum.NotFound
+	_ = interfaces.NotFound
 	_ = bind.Bind
 	_ = common.Big1
 	_ = types.BloomLookup
@@ -953,10 +953,10 @@ type TeleportMessengerAddFeeAmountIterator struct {
 	contract *bind.BoundContract // Generic contract to use for unpacking event data
 	event    string              // Event name to use for unpacking event data
 
-	logs chan types.Log        // Log channel receiving the found contract events
-	sub  ethereum.Subscription // Subscription for errors, completion and termination
-	done bool                  // Whether the subscription completed delivering logs
-	fail error                 // Occurred error to stop iteration
+	logs chan types.Log          // Log channel receiving the found contract events
+	sub  interfaces.Subscription // Subscription for errors, completion and termination
+	done bool                    // Whether the subscription completed delivering logs
+	fail error                   // Occurred error to stop iteration
 }
 
 // Next advances the iterator to the subsequent event, returning whether there
@@ -1107,10 +1107,10 @@ type TeleportMessengerFailedMessageExecutionIterator struct {
 	contract *bind.BoundContract // Generic contract to use for unpacking event data
 	event    string              // Event name to use for unpacking event data
 
-	logs chan types.Log        // Log channel receiving the found contract events
-	sub  ethereum.Subscription // Subscription for errors, completion and termination
-	done bool                  // Whether the subscription completed delivering logs
-	fail error                 // Occurred error to stop iteration
+	logs chan types.Log          // Log channel receiving the found contract events
+	sub  interfaces.Subscription // Subscription for errors, completion and termination
+	done bool                    // Whether the subscription completed delivering logs
+	fail error                   // Occurred error to stop iteration
 }
 
 // Next advances the iterator to the subsequent event, returning whether there
@@ -1261,10 +1261,10 @@ type TeleportMessengerMessageExecutionRetriedIterator struct {
 	contract *bind.BoundContract // Generic contract to use for unpacking event data
 	event    string              // Event name to use for unpacking event data
 
-	logs chan types.Log        // Log channel receiving the found contract events
-	sub  ethereum.Subscription // Subscription for errors, completion and termination
-	done bool                  // Whether the subscription completed delivering logs
-	fail error                 // Occurred error to stop iteration
+	logs chan types.Log          // Log channel receiving the found contract events
+	sub  interfaces.Subscription // Subscription for errors, completion and termination
+	done bool                    // Whether the subscription completed delivering logs
+	fail error                   // Occurred error to stop iteration
 }
 
 // Next advances the iterator to the subsequent event, returning whether there
@@ -1414,10 +1414,10 @@ type TeleportMessengerReceiveCrossChainMessageIterator struct {
 	contract *bind.BoundContract // Generic contract to use for unpacking event data
 	event    string              // Event name to use for unpacking event data
 
-	logs chan types.Log        // Log channel receiving the found contract events
-	sub  ethereum.Subscription // Subscription for errors, completion and termination
-	done bool                  // Whether the subscription completed delivering logs
-	fail error                 // Occurred error to stop iteration
+	logs chan types.Log          // Log channel receiving the found contract events
+	sub  interfaces.Subscription // Subscription for errors, completion and termination
+	done bool                    // Whether the subscription completed delivering logs
+	fail error                   // Occurred error to stop iteration
 }
 
 // Next advances the iterator to the subsequent event, returning whether there
@@ -1568,10 +1568,10 @@ type TeleportMessengerSendCrossChainMessageIterator struct {
 	contract *bind.BoundContract // Generic contract to use for unpacking event data
 	event    string              // Event name to use for unpacking event data
 
-	logs chan types.Log        // Log channel receiving the found contract events
-	sub  ethereum.Subscription // Subscription for errors, completion and termination
-	done bool                  // Whether the subscription completed delivering logs
-	fail error                 // Occurred error to stop iteration
+	logs chan types.Log          // Log channel receiving the found contract events
+	sub  interfaces.Subscription // Subscription for errors, completion and termination
+	done bool                    // Whether the subscription completed delivering logs
+	fail error                   // Occurred error to stop iteration
 }
 
 // Next advances the iterator to the subsequent event, returning whether there
--- scripts/abi_go_bindings.sh
+++ scripts/abi_go_bindings.sh
@@ -15,16 +15,20 @@ if ! command -v forge &> /dev/null; then
     $BASE_DIR/.foundry/bin/foundryup
 fi
 
+echo "Building subnet-evm abi"
+cd $TELEPORTER_PATH/subnet-evm/cmd/abigen
+go install
+
 echo "Building Contracts"
 cd $TELEPORTER_PATH/contracts
-$BASE_DIR/.foundry/bin/forge build
-python3 -c "import json; json.dump(json.load(open('out/TeleporterMessenger.sol/TeleporterMessenger.json'))['abi'], open('$TELEPORTER_PATH/abis/TeleporterMessenger.json', 'w'), indent=4)"
+$BASE_DIR/.foundry/bin/forge build --extra-output-files abi
+cp $TELEPORTER_PATH/contracts/out/TeleporterMessenger.sol/TeleporterMessenger.abi.json $TELEPORTER_PATH/abis/
 
 echo "Generating TeleporterMessenger Go bindings"
 gen_file_path=$TELEPORTER_PATH/abis/go/teleport-messenger/teleport_messenger.go
 cd $TELEPORTER_PATH
 mkdir -p abis/go/teleport-messenger
-abigen --abi abis/TeleporterMessenger.json --pkg teleport_messenger --out $gen_file_path
+$GOPATH/bin/abigen --abi abis/TeleporterMessenger.abi.json --pkg teleport_messenger --out $gen_file_path
 
 echo "TeleporterMessenger Go bindings generated at $gen_file_path"
 
