6c8cf3fd3f3372f7767427e76b7baf85d74a9a5a
Matthew Lam <matthew.lam@avalabs.org>
2023-09-27 20:35:20 +0000

    add docs, license, and event

--- contracts/src/Teleporter/TeleporterRegistry.sol
+++ contracts/src/Teleporter/TeleporterRegistry.sol
@@ -1,22 +1,32 @@
 // (c) 2023, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-License-Identifier: Ecosystem
 
 pragma solidity 0.8.18;
 
 import "../WarpProtocolRegistry.sol";
 import "./ITeleporterMessenger.sol";
 
+/**
+ * @dev TeleporterRegistry contract is a {WarpProtocolRegistry} and provides an upgrade
+ * mechanism for teleporter messenger contracts.
+ */
 contract TeleporterRegistry is WarpProtocolRegistry {
     mapping(address => uint256) internal _addressToVersion;
 
+    /**
+     * @dev Gets the {ITeleporterMessenger} contract of the given `version`.
+     */
     function getTeleporter(
         uint256 version
     ) external view returns (ITeleporterMessenger) {
         return ITeleporterMessenger(_getVersionToAddress(version));
     }
 
+    /**
+     * @dev Gets the latest {ITeleporterMessenger} contract.
+     */
     function getLatestTeleporter()
         external
         view
@@ -25,12 +35,22 @@ contract TeleporterRegistry is WarpProtocolRegistry {
         return ITeleporterMessenger(_getVersionToAddress(_latestVersion));
     }
 
+    /**
+     * @dev Gets the version of the given `protocolAddress`.
+     * If `protocolAddress` is not a valid protocol address, returns 0.
+     */
     function getAddressToVersion(
         address protocolAddress
     ) external view returns (uint256) {
         return _addressToVersion[protocolAddress];
     }
 
+    /**
+     * @dev See {WarpProtocolRegistry-addProtocolVersion}
+     *
+     * Adds the new protocol version address to the `_addressToVersion` mapping
+     * so that the registry can be queried for the version of a given protocol address.
+     */
     function _addProtocolVersion() internal override {
         super._addProtocolVersion();
         _addressToVersion[
--- contracts/src/WarpProtocolRegistry.sol
+++ contracts/src/WarpProtocolRegistry.sol
@@ -1,14 +1,28 @@
 // (c) 2023, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-License-Identifier: Ecosystem
 
 pragma solidity 0.8.18;
 
 import "@subnet-evm-contracts/interfaces/IWarpMessenger.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
+/**
+ * @dev Implementation of an abstract `WarpProtocolRegistry` contract.
+ *
+ * This implementation is a contract that can be used as a base contract for protocols that are
+ * build on top of Warp. It allows the protocol to be upgraded through a warp out of band message.
+ */
 abstract contract WarpProtocolRegistry {
+    /**
+     * @dev Emitted when a new protocol version is added to the registry.
+     */
+    event AddProtocolVersion(
+        uint256 indexed version,
+        address indexed protocolAddress
+    );
+
     // Address that the out-of-band warp message sets as the "source" address.
     // The address is obviously not owned by any EOA or smart contract account, so it
     // can not possibly be the source address of any other warp message emitted by the VM.
@@ -32,21 +46,44 @@ abstract contract WarpProtocolRegistry {
     error InvalidProtocolAddress();
     error InvalidProtocolVersion();
 
+    /**
+     * @dev Initializes the contract by setting a `chainID` and `latestVersion`.
+     */
     constructor() {
         _latestVersion = 0;
         _chainID = WARP_MESSENGER.getBlockchainID();
     }
 
+    /**
+     * @dev Gets and verifies a warp out of band message, and adds the new protocol version
+     * addres to the registry.
+     *
+     * Emits a {AddProtocolVersion} event when successful.
+     * Requirements:
+     *
+     * - a valid warp out of band message must be provided.
+     * - the version must be the increment of the latest version.
+     * - the protocol address must be a contract address.
+     */
     function addProtocolVersion() external {
         _addProtocolVersion();
     }
 
+    /**
+     * @dev Gets the address of a protocol version.
+     * Requirements:
+     *
+     * - the version must be a valid version.
+     */
     function getVersionToAddress(
         uint256 version
     ) external view returns (address) {
         return _getVersionToAddress(version);
     }
 
+    /**
+     * @dev Gets the latest protocol version.
+     */
     function getLatestVersion() external view returns (uint256) {
         return _latestVersion;
     }
@@ -84,6 +121,7 @@ abstract contract WarpProtocolRegistry {
 
         _latestVersion++;
         _versionToAddress[_latestVersion] = protocolAddress;
+        emit AddProtocolVersion(_latestVersion, protocolAddress);
     }
 
     function _getVersionToAddress(
