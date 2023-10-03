175b4449a46ca385e69e7b0a5b31fe6ee4e8eb61
Gwen <linguan.yang@avalabs.org>
2023-10-03 10:45:39 -0400

    fix unit tests lint

--- contracts/src/Teleporter/tests/ReceiptsQueueTests.t.sol
+++ contracts/src/Teleporter/tests/ReceiptsQueueTests.t.sol
@@ -13,93 +13,95 @@ contract ReceiptQueueTest is Test {
     // The state of the contract gets reset before each
     // test is run, with the `setUp()` function being called
     // each time after deployment.
-    ReceiptQueue.TeleporterMessageReceiptQueue public queue;
+    ReceiptQueue.TeleporterMessageReceiptQueue private _queue;
 
      // Add 3 elements to the queue.
-    TeleporterMessageReceipt private receipt1 = TeleporterMessageReceipt({
+    TeleporterMessageReceipt private _receipt1 = TeleporterMessageReceipt({
         receivedMessageID: 543,
         relayerRewardAddress: 0x10eB43ef5982628728E3E4bb9F78834f67Fbb40b
     });
-    TeleporterMessageReceipt private receipt2 = TeleporterMessageReceipt({
+    TeleporterMessageReceipt private _receipt2 = TeleporterMessageReceipt({
         receivedMessageID: 684384,
         relayerRewardAddress: 0x10eB43ef5982628728E3E4bb9F78834f67Fbb40b
     });
-    TeleporterMessageReceipt private receipt3 = TeleporterMessageReceipt({
+    TeleporterMessageReceipt private _receipt3 = TeleporterMessageReceipt({
         receivedMessageID: 654351,
         relayerRewardAddress: 0xcC8E718045817AebA89592C72Ae1C9917f5D0894
     });
 
     function testReceiptQueue() public {
         // Check the initial size is zero.
-        assertEq(queue.size(), 0);
+        assertEq(_queue.size(), 0);
 
-        queue.enqueue(receipt1);
-        queue.enqueue(receipt2);
-        queue.enqueue(receipt3);
+        _queue.enqueue(_receipt1);
+        _queue.enqueue(_receipt2);
+        _queue.enqueue(_receipt3);
 
         // Check the size is now three.
-        assertEq(queue.size(), 3);
+        assertEq(_queue.size(), 3);
 
         // Dequeue 3 elements and check they are given in the correct order (FIFO).
-        TeleporterMessageReceipt memory result = queue.dequeue();
-        assertEq(result.receivedMessageID, receipt1.receivedMessageID);
-        assertEq(result.relayerRewardAddress, receipt1.relayerRewardAddress);
-        result = queue.dequeue();
-        assertEq(result.receivedMessageID, receipt2.receivedMessageID);
-        assertEq(result.relayerRewardAddress, receipt2.relayerRewardAddress);
-        result = queue.dequeue();
-        assertEq(result.receivedMessageID, receipt3.receivedMessageID);
-        assertEq(result.relayerRewardAddress, receipt3.relayerRewardAddress);
+        TeleporterMessageReceipt memory result = _queue.dequeue();
+        assertEq(result.receivedMessageID, _receipt1.receivedMessageID);
+        assertEq(result.relayerRewardAddress, _receipt1.relayerRewardAddress);
+        result = _queue.dequeue();
+        assertEq(result.receivedMessageID, _receipt2.receivedMessageID);
+        assertEq(result.relayerRewardAddress, _receipt2.relayerRewardAddress);
+        result = _queue.dequeue();
+        assertEq(result.receivedMessageID, _receipt3.receivedMessageID);
+        assertEq(result.relayerRewardAddress, _receipt3.relayerRewardAddress);
 
         // Check the size is now 0 again.
-        assertEq(queue.size(), 0);
+        assertEq(_queue.size(), 0);
 
         // Enqueue two more of the same item to check you can have duplicates, followed by the second and third.
-        queue.enqueue(receipt1);
-        queue.enqueue(receipt1);
-        queue.enqueue(receipt2);
-        queue.enqueue(receipt3);
+        _queue.enqueue(_receipt1);
+        _queue.enqueue(_receipt1);
+        _queue.enqueue(_receipt2);
+        _queue.enqueue(_receipt3);
 
         // Check the size again.
-        assertEq(queue.size(), 4);
+        assertEq(_queue.size(), 4);
 
         // Finally dequeue the elements and once again check they are returned in the correct order.
-        result = queue.dequeue();
-        assertEq(result.receivedMessageID, receipt1.receivedMessageID);
-        assertEq(result.relayerRewardAddress, receipt1.relayerRewardAddress);
-        result = queue.dequeue();
-        assertEq(result.receivedMessageID, receipt1.receivedMessageID);
-        assertEq(result.relayerRewardAddress, receipt1.relayerRewardAddress);
-        result = queue.dequeue();
-        assertEq(result.receivedMessageID, receipt2.receivedMessageID);
-        assertEq(result.relayerRewardAddress, receipt2.relayerRewardAddress);
-        result = queue.dequeue();
-        assertEq(result.receivedMessageID, receipt3.receivedMessageID);
-        assertEq(result.relayerRewardAddress, receipt3.relayerRewardAddress);
+        result = _queue.dequeue();
+        assertEq(result.receivedMessageID, _receipt1.receivedMessageID);
+        assertEq(result.relayerRewardAddress, _receipt1.relayerRewardAddress);
+        result = _queue.dequeue();
+        assertEq(result.receivedMessageID, _receipt1.receivedMessageID);
+        assertEq(result.relayerRewardAddress, _receipt1.relayerRewardAddress);
+        result = _queue.dequeue();
+        assertEq(result.receivedMessageID, _receipt2.receivedMessageID);
+        assertEq(result.relayerRewardAddress, _receipt2.relayerRewardAddress);
+        result = _queue.dequeue();
+        assertEq(result.receivedMessageID, _receipt3.receivedMessageID);
+        assertEq(result.relayerRewardAddress, _receipt3.relayerRewardAddress);
     }
 
     function testDequeueEmptyQueue() public {
         // Check that you can't dequeue from empty queue.
         vm.expectRevert(ReceiptQueue.EmptyQueue.selector);
-        queue.dequeue();
+        TeleporterMessageReceipt memory result = _queue.dequeue();
+        assertEq(result.receivedMessageID, 0);
+        assertEq(result.relayerRewardAddress, address(0));
     }
 
     function testGetReceiptAtIndex() public {
-        queue.enqueue(receipt1);
-        queue.enqueue(receipt1);
-        queue.enqueue(receipt2);
-        queue.enqueue(receipt3);
+        _queue.enqueue(_receipt1);
+        _queue.enqueue(_receipt1);
+        _queue.enqueue(_receipt2);
+        _queue.enqueue(_receipt3);
 
         // Check the size again.
-        assertEq(queue.size(), 4);
+        assertEq(_queue.size(), 4);
 
         // Check that you can get receipts at specific indexes.
-        TeleporterMessageReceipt memory result = queue.getReceiptAtIndex(2);
-        assertEq(result.receivedMessageID, receipt2.receivedMessageID);
-        assertEq(result.relayerRewardAddress, receipt2.relayerRewardAddress);
+        TeleporterMessageReceipt memory result = _queue.getReceiptAtIndex(2);
+        assertEq(result.receivedMessageID, _receipt2.receivedMessageID);
+        assertEq(result.relayerRewardAddress, _receipt2.relayerRewardAddress);
 
         // Check  can't get an out of index element.
         vm.expectRevert(ReceiptQueue.OutofIndex.selector);
-        result = queue.getReceiptAtIndex(4);
+        result = _queue.getReceiptAtIndex(4);
     }
 }
