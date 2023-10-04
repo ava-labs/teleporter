37902b3f75d865a4b3ac95fb7f59692679ecd195
Gwen <linguan.yang@avalabs.org>
2023-10-04 10:11:24 -0400

    add comments

--- .github/workflows/check_clean_branch.sh
+++ .github/workflows/check_clean_branch.sh
@@ -5,5 +5,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Modifies the index based on the current index.
+# Checks to see if merges or updates are needed by checking stat() information.
 git update-index --really-refresh > /dev/null
+# Checks to see if there are any differences from the index to the current HEAD commit
 git diff-index --quiet HEAD
