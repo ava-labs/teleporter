2f526ded74ee0ca68ae1a52da7eec79b55040420
Matthew Lam <matthew.lam@avalabs.org>
2023-10-18 23:43:00 +0000

    add additional requirements to doc

--- contracts/src/WarpProtocolRegistry.sol
+++ contracts/src/WarpProtocolRegistry.sol
@@ -71,9 +71,14 @@ abstract contract WarpProtocolRegistry {
      * Emits a {AddProtocolVersion} event when successful.
      * Requirements:
      *
-     * - a valid warp out-of-band message must be provided.
+     * - a valid Warp out-of-band message must be provided.
+     * - Message source chain ID must be the same as the blockchain ID of the registry.
+     * - Message origin sender address must be the same as the `VALIDATORS_SOURCE_ADDRESS`.
+     * - Message destination chain ID must be the same as the blockchain ID of the registry.
+     * - Message destination address must be the same as the address of the registry.
+     * - the version must not be zero.
      * - the version must not already be registered.
-     * - version and protocol address can not be the zero values.
+     * - protocol address must not be zero address.
      */
     function addProtocolVersion(uint32 messageIndex) external virtual {
         // Get and validate for a warp out-of-band message.
