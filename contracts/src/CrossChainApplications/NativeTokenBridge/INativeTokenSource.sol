293570c62a1ad73c656acfee598189e6554254b6
Geoff Stuart <geoff.vball@gmail.com>
2023-10-29 18:43:59 -0400

    Update contracts/src/CrossChainApplications/NativeTokenBridge/INativeTokenSource.sol
    Co-authored-by: minghinmatthewlam <matthew.lam@avalabs.org>
    Signed-off-by: Geoff Stuart <geoff.vball@gmail.com>

--- contracts/src/CrossChainApplications/NativeTokenBridge/INativeTokenSource.sol
+++ contracts/src/CrossChainApplications/NativeTokenBridge/INativeTokenSource.sol
@@ -7,7 +7,7 @@ pragma solidity 0.8.18;
 
 /**
  * @dev Interface that describes functionalities for a contract that will lock tokens and send a
- * Teleporter message to a NativeTokenDestination contract to mint native tokens on that chain.
+ * Teleporter message to a {INativeTokenDestination} contract to mint native tokens on the destination chain.
  */
 interface INativeTokenSource {
     /**
