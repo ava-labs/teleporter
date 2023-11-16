0bbb1b7ba43006ab451a0f50f4a3cd725f7319cf
Geoff Stuart <geoff.vball@gmail.com>
2023-11-16 12:37:41 -0500

    Update contracts/src/CrossChainApplications/NativeTokenBridge/IERC20TokenSource.sol
    Co-authored-by: minghinmatthewlam <matthew.lam@avalabs.org>
    Signed-off-by: Geoff Stuart <geoff.vball@gmail.com>

--- contracts/src/CrossChainApplications/NativeTokenBridge/IERC20TokenSource.sol
+++ contracts/src/CrossChainApplications/NativeTokenBridge/IERC20TokenSource.sol
@@ -7,7 +7,7 @@ pragma solidity 0.8.18;
 
 /**
  * @dev Interface that describes functionalities for a contract that will lock ERC20 tokens and send a
- * Teleporter message to a NativeTokenDestination contract to mint native tokens on that chain.
+ * Teleporter message to a {INativeTokenDestination} contract to mint native tokens on that chain.
  */
 interface IERC20TokenSource {
     /**
