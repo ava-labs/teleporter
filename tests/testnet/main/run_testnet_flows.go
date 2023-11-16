6b422c9710171ee253356fce3d1565892574a6a9
Michael Kaplan <michael@avalabs.org>
2023-11-16 17:22:59 -0500

    Run all test flows against testnet

--- tests/testnet/main/run_testnet_flows.go
+++ tests/testnet/main/run_testnet_flows.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/ava-labs/teleporter/tests/flows"
 	"github.com/ava-labs/teleporter/tests/testnet"
 	"github.com/onsi/gomega"
@@ -18,6 +20,43 @@ func main() {
 		panic(err)
 	}
 
-	// Run the test, composing it with the Network implementation
+	// Run the Teleporter test flows.
 	flows.BasicOneWaySend(network)
+	fmt.Println("Finished running test BasicOneWaySend")
+
+	flows.DeliverToWrongChain(network)
+	fmt.Println("Finished running test DeliverToWrongChain")
+
+	flows.DeliverToNonExistentContract(network)
+	fmt.Println("Finished running test DeliverToNonExistentContract")
+
+	flows.RetrySuccessfulExecution(network)
+	fmt.Println("Finished running test RetrySuccessfulExecution")
+
+	flows.UnallowedRelayer(network)
+	fmt.Println("Finished running test UnallowedRelayer")
+
+	flows.ReceiveMessageTwice(network)
+	fmt.Println("Finished running test ReceiveMessageTwice")
+
+	flows.AddFeeAmount(network)
+	fmt.Println("Finished running test AddFeeAmount")
+
+	flows.InsufficientGas(network)
+	fmt.Println("Finished running test InsufficientGas")
+
+	flows.ResubmitAlteredMessage(network)
+	fmt.Println("Finished running test ResubmitAlteredMessage")
+
+	flows.RelayerModifiesMessage(network)
+	fmt.Println("Finished running test RelayerModifiesMessage")
+
+	fmt.Println("Finished Teleporter test flows.")
+
+	// Run the cross-chain application test flows.
+	flows.ExampleMessenger(network)
+	fmt.Println("Finished running test ExampleMessenger")
+
+	fmt.Println("Finished cross-chain application test flows.")
+
 }
