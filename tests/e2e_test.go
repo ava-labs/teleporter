8d12650aed697c5e3bb5a5f743bc05f43e75d2ef
Gwen <linguan.yang@avalabs.org>
2023-11-17 09:53:23 -0500

    fix

--- tests/e2e_test.go
+++ tests/e2e_test.go
@@ -51,8 +51,11 @@ var _ = ginkgo.BeforeSuite(func() {
 var _ = ginkgo.AfterSuite(localUtils.TearDownNetwork)
 
 var _ = ginkgo.Describe("[Teleporter integration tests]", func() {
-	// Teleporter tests
+	// Cross-chain application tests
+	ginkgo.It("Example cross chain messenger", ExampleMessengerGinkgo)
 	ginkgo.It("ERC20 bridge multihop", ERC20BridgeMultihopGinkgo)
+
+	// Teleporter tests
 	ginkgo.It("Send a message from Subnet A to Subnet B", BasicOneWaySendGinkgo)
 	ginkgo.It("Deliver to the wrong chain", DeliverToWrongChainGinkgo)
 	ginkgo.It("Deliver to non-existent contract", DeliverToNonExistentContractGinkgo)
@@ -65,7 +68,4 @@ var _ = ginkgo.Describe("[Teleporter integration tests]", func() {
 	ginkgo.It("Resubmit altered message", ResubmitAlteredMessageGinkgo)
 	ginkgo.It("Relayer modifies message", RelayerModifiesMessageGinkgo)
 	ginkgo.It("Validator churn", ValidatorChurnGinkgo)
-
-	// Cross-chain application tests
-	ginkgo.It("Example cross chain messenger", ExampleMessengerGinkgo)
 })
