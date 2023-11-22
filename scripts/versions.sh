8e7c77b032e556806b4440830f23e7e38d0426b2
Geoff Stuart <geoff.vball@gmail.com>
2023-11-22 10:10:45 -0500

    fix

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -19,11 +19,12 @@ function getDepVersion() {
     grep -m1 "^\s*$1" $TELEPORTER_PATH/go.mod | cut -d ' ' -f2
 }
 
-# AWM_RELAYER is needed for the bash-script integration tests, which should eventually be removed.
+# AWM_RELAYER is needed for the bash script integration tests, which uses a relayer instance to relay messages.
+# It is not included in go.mod because AWM Relayer depends on the Teleporter repo, and this would create a circular dependency.
 AWM_RELAYER_VERSION=${AWM_RELAYER_VERSION:-'v0.2.3'}
 
 # This needs to be exported to be picked up by the dockerfile.
-GO_VERSION=${GO_VERSION:-$(getDepVersion go).$GO_PATCH_VERSION}
+export GO_VERSION=${GO_VERSION:-$(getDepVersion go).$GO_PATCH_VERSION}
 
 # Don't export them as they're used in the context of other calls
 AVALANCHE_VERSION=${AVALANCHE_VERSION:-$(getDepVersion github.com/ava-labs/avalanchego)}
