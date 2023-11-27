ded99a3b6190b949c015c715ff45fb274835954c
Geoff Stuart <geoff.vball@gmail.com>
2023-11-27 13:09:23 -0500

    fix

--- scripts/install_foundry.sh
+++ scripts/install_foundry.sh
@@ -10,7 +10,9 @@ FOUNDRY_VERSION=v0.1.0
 curl -L https://raw.githubusercontent.com/ava-labs/foundry/${FOUNDRY_VERSION}/foundryup/install > /tmp/foundry-install-script
 sed -i 's/\/ava-labs\/foundry\/master\/foundryup/\/ava-labs\/foundry\/${FOUNDRY_VERSION}\/foundryup/g' /tmp/foundry-install-script
 cat /tmp/foundry-install-script | bash
-echo "export PATH=\"$PATH:$HOME/.foundry/bin\"">> ~/.bashrc
+echo "export PATH=\"$PATH:$HOME/.foundry/bin\"" >> ~/.bashrc
 source ~/.bashrc
 export PATH=$PATH:$HOME/.foundry/bin:$HOME/.cargo/bin
+which forge
+which foundryup
 foundryup --version ${FOUNDRY_VERSION}
