634afcec488eb829cd2e7991073b8aa74c8f10ef
Geoff Stuart <geoff.vball@gmail.com>
2023-11-27 10:30:44 -0500

    fix

--- scripts/install_foundry.sh
+++ scripts/install_foundry.sh
@@ -9,6 +9,6 @@ curl -L https://raw.githubusercontent.com/ava-labs/foundry/${FOUNDRY_VERSION}/fo
 sed -i "s/\/ava-labs\/foundry\/master\/foundryup/\/ava-labs\/foundry\/${FOUNDRY_VERSION}\/foundryup/g" /tmp/foundry-install-script
 cat /tmp/foundry-install-script | bash
 echo "export PATH=\"$PATH:/$HOME/.foundry/bin\"">> ~/.bashrc
-source ~/.bashrc
+source $HOME/.bashrc
 export PATH=$PATH:$HOME/.foundry/bin:$HOME/.cargo/bin
 foundryup --version ${FOUNDRY_VERSION}
