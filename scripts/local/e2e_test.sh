fc4aa309e2bc0ba8ab3600dd342f43f0c94de40d
minghinmatthewlam <matthew.lam@avalabs.org>
2023-11-28 13:14:41 -0800

    Update scripts/local/e2e_test.sh
    Co-authored-by: Michael Kaplan <55204436+michaelkaplan13@users.noreply.github.com>
    Signed-off-by: minghinmatthewlam <matthew.lam@avalabs.org>

--- scripts/local/e2e_test.sh
+++ scripts/local/e2e_test.sh
@@ -15,7 +15,7 @@ source "$TELEPORTER_PATH"/scripts/versions.sh
 BASEDIR=${BASEDIR:-"~/tmp/e2e-test"}
 
 cwd=$(pwd)
-# Install the avalanchego and subnetevm binaries
+# Install the avalanchego and subnet-evm binaries
 BASEDIR=$BASEDIR AVALANCHEGO_BUILD_PATH=$BASEDIR/avalanchego ./scripts/install_avalanchego_release.sh
 BASEDIR=$BASEDIR ./scripts/install_subnetevm_release.sh
 
