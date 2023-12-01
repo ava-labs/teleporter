813c32f0a7043554a8dc45e954e2fbdb6d9cf54f
Michael Kaplan <michael@avalabs.org>
2023-12-01 16:31:01 -0500

    Fix tmp installation directory used in local e2e script

--- scripts/local/e2e_test.sh
+++ scripts/local/e2e_test.sh
@@ -12,7 +12,7 @@ TELEPORTER_PATH=$(
 source "$TELEPORTER_PATH"/scripts/constants.sh
 source "$TELEPORTER_PATH"/scripts/versions.sh
 
-BASEDIR=${BASEDIR:-"~/tmp/e2e-test"}
+BASEDIR=${BASEDIR:-"/tmp/e2e-test"}
 
 cwd=$(pwd)
 # Install the avalanchego and subnet-evm binaries
