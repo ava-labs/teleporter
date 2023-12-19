6364e01f311244742a2ea00308e634ac3039bc32
Geoff Stuart <geoff.vball@gmail.com>
2023-12-19 01:31:28 -0500

    Abi bindings and stub

--- tests/testnet/network.go
+++ tests/testnet/network.go
@@ -56,6 +56,12 @@ type testNetwork struct {
 	fundedKey                 *ecdsa.PrivateKey
 }
 
+// GetPrimaryNetworkInfo implements interfaces.Network.
+func (*testNetwork) GetPrimaryNetworkInfo() interfaces.SubnetTestInfo {
+	// TODO
+	panic("unimplemented")
+}
+
 func initializeSubnetInfo(
 	subnetPrefix string,
 	teleporterContractAddress common.Address,
