836556f0f8329595588d3b014eedb429e5d9651d
Geoff Stuart <geoff.vball@gmail.com>
2024-03-12 16:23:37 -0400

    Update scripts/constants.sh
    Co-authored-by: minghinmatthewlam <matthew.lam@avalabs.org>
    Signed-off-by: Geoff Stuart <geoff.vball@gmail.com>

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+# Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
 # See the file LICENSE for licensing terms.
 
 # Use lower_case variables in the scripts and UPPER_CASE variables for override
