faf0119ce78df7a764086a0e156e406b43c8ce2b
minghinmatthewlam <matthew.lam@avalabs.org>
2024-05-16 15:08:40 -0700

    Update contracts/src/ERC20Source.sol
    Co-authored-by: cam-schultz <78878559+cam-schultz@users.noreply.github.com>
    Signed-off-by: minghinmatthewlam <matthew.lam@avalabs.org>

--- contracts/src/ERC20Source.sol
+++ contracts/src/ERC20Source.sol
@@ -41,7 +41,7 @@ contract ERC20Source is IERC20Source, TeleporterTokenSource {
      * @notice Initializes this source token bridge instance to send ERC20
      * tokens to other destination token bridges.
      * @param teleporterRegistryAddress The current blockchain id's Teleporter registry
-     * address. https://github.com/ava-labs/teleporter/tree/main/contracts/src/Teleporter/upgrades
+     * address. See here for details: https://github.com/ava-labs/teleporter/tree/main/contracts/src/Teleporter/upgrades
      * @param teleporterManager Address that manages this contract's integration with the
      * Teleporter registry and Teleporter versions.
      * @param tokenAddress The ERC20 token contract address to bridge to the destination chain
