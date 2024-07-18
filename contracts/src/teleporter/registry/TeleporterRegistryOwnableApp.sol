df4a3db2a189df2f719905049442fdac0f1da1da
Matthew Lam <matthew.lam@avalabs.org>
2024-07-18 12:25:21 -0400

    draft version

--- contracts/src/teleporter/registry/TeleporterRegistryOwnableApp.sol
+++ contracts/src/teleporter/registry/TeleporterRegistryOwnableApp.sol
@@ -5,11 +5,39 @@
 
 pragma solidity 0.8.20;
 
-import {TeleporterRegistryOwnableAppUpgradeable} from
-    "./TeleporterRegistryOwnableAppUpgradeable.sol";
+import {TeleporterRegistryApp} from "./TeleporterRegistryApp.sol";
+import {Ownable} from "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
+import {Context} from "@openzeppelin/contracts@5.0.2/utils/Context.sol";
+import {ContextUpgradeable} from
+    "@openzeppelin/contracts-upgradeable@5.0.2/utils/ContextUpgradeable.sol";
 
-abstract contract TeleporterRegistryOwnableApp is TeleporterRegistryOwnableAppUpgradeable {
-    constructor(address teleporterRegistryAddress, address initialOwner) {
-        __TeleporterRegistryOwnableAppUpgradeable_init(teleporterRegistryAddress, initialOwner);
+abstract contract TeleporterRegistryOwnableApp is TeleporterRegistryApp, Ownable {
+    constructor(
+        address teleporterRegistryAddress,
+        address initialOwner
+    ) TeleporterRegistryApp(teleporterRegistryAddress) Ownable(initialOwner) {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    function _contextSuffixLength()
+        internal
+        view
+        override (ContextUpgradeable, Context)
+        returns (uint256)
+    {
+        return Context._contextSuffixLength();
+    }
+
+    function _msgData()
+        internal
+        view
+        override (ContextUpgradeable, Context)
+        returns (bytes calldata)
+    {
+        return Context._msgData();
+    }
+
+    function _msgSender() internal view override (ContextUpgradeable, Context) returns (address) {
+        return Context._msgSender();
     }
 }
