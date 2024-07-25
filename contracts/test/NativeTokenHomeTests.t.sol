6763c748d9ce52d69f2d3e357ec0f7c76fe49a17
minghinmatthewlam <matthew.lam@avalabs.org>
2024-07-25 11:15:04 -0400

    Update contracts/test/NativeTokenHomeTests.t.sol
    Co-authored-by: Michael Kaplan <55204436+michaelkaplan13@users.noreply.github.com>
    Signed-off-by: minghinmatthewlam <matthew.lam@avalabs.org>

--- contracts/test/NativeTokenHomeTests.t.sol
+++ contracts/test/NativeTokenHomeTests.t.sol
@@ -28,8 +28,7 @@ contract NativeTokenHomeTest is NativeTokenTransferrerTest, TokenHomeTest {
     function setUp() public override {
         TokenHomeTest.setUp();
 
-        WrappedNativeToken token = new WrappedNativeToken("AVAX");
-        wavax = token;
+        wavax = new WrappedNativeToken("AVAX");
         app = new NativeTokenHome();
         app.initialize(
             MOCK_TELEPORTER_REGISTRY_ADDRESS, MOCK_TELEPORTER_MESSENGER_ADDRESS, address(wavax)
