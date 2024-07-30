36a8e0fed4b87274ebccf9fff83d4950392f6b55
cam-schultz <cameron.schultz@avalabs.org>
2024-07-30 13:45:37 -0400

    unit test skeleton

---
+++ contracts/src/staking/tests/NativeTokenStakingManagerTests.t.sol
@@ -0,0 +1,41 @@
+// (c) 2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// SPDX-License-Identifier: Ecosystem
+
+pragma solidity 0.8.25;
+
+import {Test} from "forge-std/Test.sol";
+import {StakingManagerTest} from "./StakingManagerTests.t.sol";
+import {NativeTokenStakingManager} from "../NativeTokenStakingManager.sol";
+import {
+    StakingManagerSettings,
+    InitialStakerInfo
+} from "../interfaces/IStakingManager.sol";
+import {
+    IRewardCalculator
+} from "../interfaces/IRewardCalculator.sol";
+
+contract NativeTokenStakingManagerTest is StakingManagerTest {
+    NativeTokenStakingManager public nativeTokenStakingManager;
+
+    function setup() public override {
+        StakingManagerTest.setup();
+        nativeTokenStakingManager = new NativeTokenStakingManager();
+        // TODONOW: Use real values
+        nativeTokenStakingManager.initialize(
+            StakingManagerSettings({
+                pChainBlockchainID: "P-Chain",
+                subnetID: "X",
+                minimumStakeAmount: 1000,
+                maximumStakeAmount: 1000000,
+                minimumStakeDuration: 24 hours,
+                maximumHourlyChurn: 10,
+                initialStakers: new InitialStakerInfo[](0),
+                rewardCalculator: IRewardCalculator(address(0))
+            })
+        );
+    }
+
+    function testInitializeValidatorRegistration() public {}
+}
---
+++ contracts/src/staking/tests/StakingManagerTests.t.sol
@@ -0,0 +1,58 @@
+// (c) 2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// SPDX-License-Identifier: Ecosystem
+
+pragma solidity 0.8.25;
+
+import {Test} from "forge-std/Test.sol";
+import {
+    StakingManager
+} from "../StakingManager.sol";
+import {
+    IStakingManager
+} from "../interfaces/IStakingManager.sol";
+
+abstract contract StakingManagerTest is Test {
+    IStakingManager public stakingManager;
+
+    event ValidationPeriodCreated(
+        bytes32 indexed validationID,
+        bytes32 indexed nodeID,
+        bytes32 indexed registerValidationMessageID,
+        uint256 weight,
+        uint64 registrationExpiry
+    );
+
+    event ValidationPeriodRegistered(
+        bytes32 indexed validationID, uint256 stakeAmount, uint256 timestamp
+    );
+
+    event ValidatorRemovalInitialized(
+        bytes32 indexed validationID,
+        bytes32 indexed setWeightMessageID,
+        uint256 stakeAmount,
+        uint256 endTime,
+        uint64 uptime
+    );
+
+    event ValidationPeriodEnded(bytes32 indexed validationID);
+
+    function setup() public virtual {}
+
+    function testProvideInitialStake() public {}
+
+    function testResendRegisterValidatorMessage() public {}
+
+    function testCompleteValidatorRegistration() public {}
+
+    function testInitializeEndValidation() public {}
+
+    function testResendEndValidation() public {}
+
+    function testCompleteEndValidation() public {}
+
+    function testValueToWeight() public {}
+
+    function testWeightToValue() public {}
+}
