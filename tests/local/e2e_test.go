f6a80863657acf8945fe09e0b4061cdfaf362b0c
Michael Kaplan <michael@avalabs.org>
2024-08-01 12:17:31 -0400

    Remove manual teleporter deployment

--- tests/local/e2e_test.go
+++ tests/local/e2e_test.go
@@ -58,24 +58,8 @@ var _ = ginkgo.BeforeSuite(func() {
 		},
 		2,
 	)
-
 	log.Info("Startied local network")
-	// Generate the Teleporter deployment values
-	// teleporterDeployerTransaction, teleporterDeployerAddress, teleporterContractAddress, err :=
-	// 	deploymentUtils.ConstructKeylessTransaction(
-	// 		teleporterByteCodeFile,
-	// 		false,
-	// 		deploymentUtils.GetDefaultContractCreationGasPrice(),
-	// 	)
-	// Expect(err).Should(BeNil())
 
-	// LocalNetworkInstance.DeployTeleporterContracts(
-	// 	teleporterDeployerTransaction,
-	// 	teleporterDeployerAddress,
-	// 	teleporterContractAddress,
-	// 	fundedKey,
-	// 	true,
-	// )
 	teleporterContractAddress := common.HexToAddress("0x253b2784c75e510dD0fF1da844684a1aC0aa5fcf")
 	_, fundedKey := LocalNetworkInstance.GetFundedAccountInfo()
 	LocalNetworkInstance.DeployTeleporterRegistryContracts(teleporterContractAddress, fundedKey)
