662b0d2cca6be7877b84962b0c13e5df92013a59
cam-schultz <cameron.schultz@avalabs.org>
2024-08-15 16:24:52 -0400

    enable all tests

--- tests/local/e2e_test.go
+++ tests/local/e2e_test.go
@@ -108,93 +108,93 @@ var _ = ginkgo.Describe("[Teleporter integration tests]", func() {
 		func() {
 			flows.NativeTokenStakingManager(LocalNetworkInstance)
 		})
-	// ginkgo.It("Send a message from Subnet A to Subnet B, and one from B to A",
-	// 	ginkgo.Label(teleporterMessengerLabel),
-	// 	func() {
-	// 		flows.BasicSendReceive(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Deliver to the wrong chain",
-	// 	ginkgo.Label(teleporterMessengerLabel),
-	// 	func() {
-	// 		flows.DeliverToWrongChain(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Deliver to non-existent contract",
-	// 	ginkgo.Label(teleporterMessengerLabel),
-	// 	func() {
-	// 		flows.DeliverToNonExistentContract(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Retry successful execution",
-	// 	ginkgo.Label(teleporterMessengerLabel),
-	// 	func() {
-	// 		flows.RetrySuccessfulExecution(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Unallowed relayer",
-	// 	ginkgo.Label(teleporterMessengerLabel),
-	// 	func() {
-	// 		flows.UnallowedRelayer(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Relay message twice",
-	// 	ginkgo.Label(teleporterMessengerLabel),
-	// 	func() {
-	// 		flows.RelayMessageTwice(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Add additional fee amount",
-	// 	ginkgo.Label(teleporterMessengerLabel),
-	// 	func() {
-	// 		flows.AddFeeAmount(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Send specific receipts",
-	// 	ginkgo.Label(teleporterMessengerLabel),
-	// 	func() {
-	// 		flows.SendSpecificReceipts(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Insufficient gas",
-	// 	ginkgo.Label(teleporterMessengerLabel),
-	// 	func() {
-	// 		flows.InsufficientGas(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Resubmit altered message",
-	// 	ginkgo.Label(teleporterMessengerLabel),
-	// 	func() {
-	// 		flows.ResubmitAlteredMessage(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Check upgrade access",
-	// 	ginkgo.Label(upgradabilityLabel),
-	// 	func() {
-	// 		flows.CheckUpgradeAccess(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Pause and Unpause Teleporter",
-	// 	ginkgo.Label(upgradabilityLabel),
-	// 	func() {
-	// 		flows.PauseTeleporter(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Calculate Teleporter message IDs",
-	// 	ginkgo.Label(utilsLabel),
-	// 	func() {
-	// 		flows.CalculateMessageID(LocalNetworkInstance)
-	// 	})
+	ginkgo.It("Send a message from Subnet A to Subnet B, and one from B to A",
+		ginkgo.Label(teleporterMessengerLabel),
+		func() {
+			flows.BasicSendReceive(LocalNetworkInstance)
+		})
+	ginkgo.It("Deliver to the wrong chain",
+		ginkgo.Label(teleporterMessengerLabel),
+		func() {
+			flows.DeliverToWrongChain(LocalNetworkInstance)
+		})
+	ginkgo.It("Deliver to non-existent contract",
+		ginkgo.Label(teleporterMessengerLabel),
+		func() {
+			flows.DeliverToNonExistentContract(LocalNetworkInstance)
+		})
+	ginkgo.It("Retry successful execution",
+		ginkgo.Label(teleporterMessengerLabel),
+		func() {
+			flows.RetrySuccessfulExecution(LocalNetworkInstance)
+		})
+	ginkgo.It("Unallowed relayer",
+		ginkgo.Label(teleporterMessengerLabel),
+		func() {
+			flows.UnallowedRelayer(LocalNetworkInstance)
+		})
+	ginkgo.It("Relay message twice",
+		ginkgo.Label(teleporterMessengerLabel),
+		func() {
+			flows.RelayMessageTwice(LocalNetworkInstance)
+		})
+	ginkgo.It("Add additional fee amount",
+		ginkgo.Label(teleporterMessengerLabel),
+		func() {
+			flows.AddFeeAmount(LocalNetworkInstance)
+		})
+	ginkgo.It("Send specific receipts",
+		ginkgo.Label(teleporterMessengerLabel),
+		func() {
+			flows.SendSpecificReceipts(LocalNetworkInstance)
+		})
+	ginkgo.It("Insufficient gas",
+		ginkgo.Label(teleporterMessengerLabel),
+		func() {
+			flows.InsufficientGas(LocalNetworkInstance)
+		})
+	ginkgo.It("Resubmit altered message",
+		ginkgo.Label(teleporterMessengerLabel),
+		func() {
+			flows.ResubmitAlteredMessage(LocalNetworkInstance)
+		})
+	ginkgo.It("Check upgrade access",
+		ginkgo.Label(upgradabilityLabel),
+		func() {
+			flows.CheckUpgradeAccess(LocalNetworkInstance)
+		})
+	ginkgo.It("Pause and Unpause Teleporter",
+		ginkgo.Label(upgradabilityLabel),
+		func() {
+			flows.PauseTeleporter(LocalNetworkInstance)
+		})
+	ginkgo.It("Calculate Teleporter message IDs",
+		ginkgo.Label(utilsLabel),
+		func() {
+			flows.CalculateMessageID(LocalNetworkInstance)
+		})
 
-	// // The following tests require special behavior by the relayer, so we only run them on a local network
-	// ginkgo.It("Relayer modifies message",
-	// 	ginkgo.Label(teleporterMessengerLabel),
-	// 	func() {
-	// 		flows.RelayerModifiesMessage(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Teleporter registry",
-	// 	ginkgo.Label(upgradabilityLabel),
-	// 	func() {
-	// 		flows.TeleporterRegistry(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Deliver ValidatorSetSig signed message",
-	// 	ginkgo.Label(validatorSetSigLabel),
-	// 	func() {
-	// 		flows.ValidatorSetSig(LocalNetworkInstance)
-	// 	})
-	// ginkgo.It("Validator churn",
-	// 	ginkgo.Label(teleporterMessengerLabel),
-	// 	func() {
-	// 		flows.ValidatorChurn(LocalNetworkInstance)
-	// 	})
+	// The following tests require special behavior by the relayer, so we only run them on a local network
+	ginkgo.It("Relayer modifies message",
+		ginkgo.Label(teleporterMessengerLabel),
+		func() {
+			flows.RelayerModifiesMessage(LocalNetworkInstance)
+		})
+	ginkgo.It("Teleporter registry",
+		ginkgo.Label(upgradabilityLabel),
+		func() {
+			flows.TeleporterRegistry(LocalNetworkInstance)
+		})
+	ginkgo.It("Deliver ValidatorSetSig signed message",
+		ginkgo.Label(validatorSetSigLabel),
+		func() {
+			flows.ValidatorSetSig(LocalNetworkInstance)
+		})
+	ginkgo.It("Validator churn",
+		ginkgo.Label(teleporterMessengerLabel),
+		func() {
+			flows.ValidatorChurn(LocalNetworkInstance)
+		})
 	// Since the validator churn test modifies the network topology, we put it last for now.
 	// It should not affect the other tests, but we get some errors if we run it before the other tests.
 	// TODO: we should fix this so that the order of the tests does not matter.
