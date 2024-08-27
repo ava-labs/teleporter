24c72c6f466d75875211b42cec590a845b935ae3
cam-schultz <cameron.schultz@avalabs.org>
2024-08-26 20:26:24 -0500

    rename blsPubKey param

--- contracts/staking/interfaces/INativeTokenStakingManager.sol
+++ contracts/staking/interfaces/INativeTokenStakingManager.sol
@@ -12,16 +12,11 @@ interface INativeTokenStakingManager is IPoSValidatorManager {
      * @notice Begins the validator registration process. Locks the provided native asset in the contract as the stake.
      * @param nodeID The node ID of the validator being registered.
      * @param registrationExpiry The time at which the reigistration is no longer valid on the P-Chain.
-     * @param signature The raw bytes of the Ed25519 signature over the concatenated bytes of
-     * [subnetID]+[nodeID]+[blsPublicKey]+[weight]+[balance]+[expiry]. This signature must correspond to the Ed25519
-     * public key that is used for the nodeID. This approach prevents NodeIDs from being unwillingly added to Subnets.
-     * balance is the minimum initial $nAVAX balance that must be attached to the validator serialized as a uint64.
-     * The signature field will be validated by the P-Chain. Implementations may choose to validate that the signature
-     * field is well-formed but it is not required.
+     * @param blsPublicKey The BLS public key of the validator.
      */
     function initializeValidatorRegistration(
         bytes32 nodeID,
         uint64 registrationExpiry,
-        bytes memory signature
+        bytes memory blsPublicKey
     ) external payable returns (bytes32 validationID);
 }
