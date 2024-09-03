6c3ef54a123c918d0d74a807b2fadae7cb40ba1e
Geoff Stuart <geoff.vball@gmail.com>
2024-09-03 10:39:37 -0400

    Fix tests

--- contracts/staking/tests/ValidatorManagerTests.t.sol
+++ contracts/staking/tests/ValidatorManagerTests.t.sol
@@ -223,13 +223,13 @@ abstract contract ValidatorManagerTest is Test {
                 blsPublicKey: blsPublicKey
             })
         );
-        vm.warp(DEFAULT_EXPIRY - 1);
+        vm.warp(registrationExpiry - 1);
         _mockSendWarpMessage(registerSubnetValidatorMessage, bytes32(0));
 
         _beforeSend(weight);
         vm.expectEmit(true, true, true, true, address(validatorManager));
         emit ValidationPeriodCreated(
-            validationID, DEFAULT_NODE_ID, bytes32(0), weight, DEFAULT_EXPIRY
+            validationID, nodeID, bytes32(0), weight, registrationExpiry
         );
 
         _initializeValidatorRegistration(nodeID, registrationExpiry, blsPublicKey, weight);
