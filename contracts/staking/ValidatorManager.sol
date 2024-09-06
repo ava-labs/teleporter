1ea4bfc0b4659b021f2f4c284a18527e57268311
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2024-09-06 10:56:30 -0400

    natspec style review comment
    Co-authored-by: minghinmatthewlam <matthew.lam@avalabs.org>
    Signed-off-by: F. Eugene Aumson <feuGeneA@users.noreply.github.com>

--- contracts/staking/ValidatorManager.sol
+++ contracts/staking/ValidatorManager.sol
@@ -269,7 +269,7 @@ abstract contract ValidatorManager is
      *  Note that this function can be used for successful validation periods that have been explicitly
      * ended by calling {initializeEndValidation} or for validation periods that never began on the P-Chain due to the
      * {registrationExpiry} being reached.
-     * @return the Validator instance representing the completed validation period
+     * @return The Validator instance representing the completed validation period
      */
     function _completeEndValidation(uint32 messageIndex) internal returns (Validator memory) {
         ValidatorManagerStorage storage $ = _getValidatorManagerStorage();
