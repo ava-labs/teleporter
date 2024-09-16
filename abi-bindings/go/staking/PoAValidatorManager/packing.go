9766b4e22e7b71a004af665e5a93f87b656bbced
Ian Suvak <ian.suvak@avalabs.org>
2024-09-16 18:58:20 -0400

    replace manual encoding with BigEndian Put methods

--- abi-bindings/go/staking/PoAValidatorManager/packing.go
+++ abi-bindings/go/staking/PoAValidatorManager/packing.go
@@ -3,27 +3,24 @@
 
 package poavalidatormanager
 
+import "encoding/binary"
+
 func (s *SubnetConversionData) Pack() ([]byte, error) {
 	packedLen := 72 + len(s.ValidatorManagerAddress) + 88*len(s.InitialValidators)
 
 	b := make([]byte, packedLen)
 	copy(b[0:32], s.ConvertSubnetTxID[:])
 	copy(b[32:64], s.BlockchainID[:])
-	lenAddress := len(s.ValidatorManagerAddress)
-	for i := 0; i < 4; i++ {
-		b[i+64] = byte(lenAddress >> uint8(8*(3-i)))
-	}
-	copy(b[68:68+lenAddress], s.ValidatorManagerAddress.Bytes())
-	lenValidators := len(s.InitialValidators)
-	for i := 0; i < 4; i++ {
-		b[i+68+lenAddress] = byte(lenValidators >> uint8(8*(3-i)))
-	}
+	// These are evm addresses and have lengths of 20 so hardcoding here
+	binary.BigEndian.PutUint32(b[64:68], uint32(20))
+	copy(b[68:88], s.ValidatorManagerAddress.Bytes())
+	binary.BigEndian.PutUint32(b[88:92], uint32(len(s.InitialValidators)))
 	for i, iv := range s.InitialValidators {
 		packedIv, err := iv.Pack()
 		if err != nil {
 			return nil, err
 		}
-		copy(b[72+lenAddress+i*88:72+lenAddress+(i+1)*88], packedIv)
+		copy(b[92+i*88:92+(i+1)*88], packedIv)
 	}
 	return b, nil
 }
@@ -31,9 +28,7 @@ func (s *SubnetConversionData) Pack() ([]byte, error) {
 func (iv *InitialValidator) Pack() ([]byte, error) {
 	b := make([]byte, 88)
 	copy(b[0:32], iv.NodeID[:])
-	for i := 0; i < 8; i++ {
-		b[i+32] = byte(iv.Weight >> uint8(8*(7-i)))
-	}
+	binary.BigEndian.PutUint64(b[32:40], uint64(iv.Weight))
 	copy(b[40:88], iv.BlsPublicKey)
 	return b, nil
 }
