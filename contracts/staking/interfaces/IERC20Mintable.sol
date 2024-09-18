9bbfde259f884748474a332914268920221d66fa
Geoff Stuart <geoff.vball@gmail.com>
2024-09-18 14:11:38 -0400

    review fixes

--- contracts/staking/interfaces/IERC20Mintable.sol
+++ contracts/staking/interfaces/IERC20Mintable.sol
@@ -7,6 +7,13 @@ pragma solidity 0.8.25;
 
 import {IERC20} from "@openzeppelin/contracts@5.0.2/token/ERC20/IERC20.sol";
 
+
 interface IERC20Mintable is IERC20 {
+    /**
+     * @notice Mint tokens to the specified address.
+     * @param account The address to mint tokens to.
+     * @param amount How many tokens to mint.
+     * @dev This function should have appropriate user controls to ensure that only the staking contract can mint.
+     */
     function mint(address account, uint256 amount) external;
 }
