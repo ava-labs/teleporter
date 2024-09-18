ebe122e6b3b87637b2821537c216a1dcfe9a10bc
Geoff Stuart <geoff.vball@gmail.com>
2024-09-18 14:21:02 -0400

    lint

--- contracts/staking/interfaces/IERC20Mintable.sol
+++ contracts/staking/interfaces/IERC20Mintable.sol
@@ -7,7 +7,6 @@ pragma solidity 0.8.25;
 
 import {IERC20} from "@openzeppelin/contracts@5.0.2/token/ERC20/IERC20.sol";
 
-
 interface IERC20Mintable is IERC20 {
     /**
      * @notice Mint tokens to the specified address.
