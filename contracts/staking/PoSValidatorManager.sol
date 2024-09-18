13e210b97c629d855315c68d6e3b2df585c3c662
Geoff Stuart <geoff.vball@gmail.com>
2024-09-18 17:02:28 -0400

    Review fixes

--- contracts/staking/PoSValidatorManager.sol
+++ contracts/staking/PoSValidatorManager.sol
@@ -54,11 +54,11 @@ abstract contract PoSValidatorManager is
         IRewardCalculator _rewardCalculator;
         /// @notice Maps the validation ID to its requirements.
         mapping(bytes32 validationID => PoSValidatorRequirements) _validatorRequirements;
-        /// @notice Maps the delegationID to the delegator information.
+        /// @notice Maps the delegation ID to the delegator information.
         mapping(bytes32 delegationID => Delegator) _delegatorStakes;
-        /// @notice Maps the delegationID to its pending staking rewards.
+        /// @notice Maps the delegation ID to its pending staking rewards.
         mapping(bytes32 delegationID => uint256) _redeemableDelegatorRewards;
-        /// @notice Maps the validator owner address to its pending staking rewards.
+        /// @notice Maps the validation ID to its pending staking rewards.
         mapping(bytes32 validationID => uint256) _redeemableValidatorRewards;
         /// @notice Saves the uptime of a pending completed or completed validation period so that delegators can collect rewards.
         mapping(bytes32 validationID => uint64) _completedValidationUptimeSeconds;
@@ -145,7 +145,11 @@ abstract contract PoSValidatorManager is
 
         Validator memory validator = _initializeEndValidation(validationID);
 
-        // Check that minimum stake duration has passed
+        if (!_isPoSValidator(validationID)) {
+            return;
+        }
+
+        // Check that minimum stake duration has passed.
         require(
             validator.endedAt
                 >= validator.startedAt + $._validatorRequirements[validationID].minStakeDuration,
@@ -483,10 +487,16 @@ abstract contract PoSValidatorManager is
         // Update the delegator status
         $._delegatorStakes[delegationID].status = DelegatorStatus.Completed;
 
-        _reward(delegator.owner, $._redeemableDelegatorRewards[delegationID]);
+        uint256 rewards = $._redeemableDelegatorRewards[delegationID];
         delete $._redeemableDelegatorRewards[delegationID];
+
+        uint256 validatorFees =
+            rewards * $._validatorRequirements[validationID].delegationFeeBips / 10000;
+        $._redeemableValidatorRewards[validationID] += validatorFees;
+
+        _reward(delegator.owner, rewards - validatorFees);
         _unlock(delegator.owner, weightToValue(delegator.weight));
-        // TODO can we remove the delegation from _delegatorStakes here?
+        delete $._delegatorStakes[delegationID];
 
         emit DelegationEnded(delegationID, validationID, nonce);
     }
