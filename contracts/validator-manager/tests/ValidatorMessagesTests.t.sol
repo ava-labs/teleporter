5a2c65ec7b8b93153f875567cafa1cab2c85f187
cam-schultz <cameron.schultz@avalabs.org>
2024-10-17 09:01:11 -0500

    test external lib

--- contracts/validator-manager/tests/ValidatorMessagesTests.t.sol
+++ contracts/validator-manager/tests/ValidatorMessagesTests.t.sol
@@ -6,7 +6,7 @@
 pragma solidity 0.8.25;
 
 import {Test} from "@forge-std/Test.sol";
-import {ValidatorMessages} from "./utils/ValidatorMessagesInternal.sol";
+import {ValidatorMessages} from "../ValidatorMessages.sol";
 import {PChainOwner} from "../interfaces/IValidatorManager.sol";
 
 contract ValidatorMessagesTest is Test {
