1591fced65881329d2b32b843e721354233fa8d8
Geoff Stuart <geoffrey.stuart@avalabs.org>
2024-10-24 10:57:41 -0400

    Type to memory

--- contracts/validator-manager/ValidatorMessages.sol
+++ contracts/validator-manager/ValidatorMessages.sol
@@ -148,7 +148,7 @@ library ValidatorMessages {
      * @param subnetConversionData The struct representing data to pack into the message.
      * @return The packed message.
      */
-    function packSubnetConversionData(SubnetConversionData calldata subnetConversionData)
+    function packSubnetConversionData(SubnetConversionData memory subnetConversionData)
         internal
         pure
         returns (bytes memory)
